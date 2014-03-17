5e62cb6be82fb0c5e7ef69f65abf62e76411a0ea
hawkeye126 <pete.lower@gmail.com>
2014-03-17 16:52:19 +0000

    adding js edits

--- lowermedia.js
+++ lowermedia.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-var wide = jQuery('#one-page-masthead').css('width');
-var calculate = parseInt(wide, 10)* 0.5;
-jQuery('#one-page-masthead').css('height', calculate);
+	var wide = jQuery('#one-page-masthead').css('width');
+	var calculate = parseInt(wide, 10)* 0.5;
+	jQuery('#one-page-masthead').css('height', calculate);
 });
